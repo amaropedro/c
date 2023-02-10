a2a3d2f527c69a0381cecfa41690d80465f1787c
Amaro <tpedro.amaro@gmail.com>
2023-02-10 17:52:11 -0300

    adds missing files

---
+++ headers/func.c
@@ -0,0 +1,19 @@
+int EDivisivel(int a, int b)
+{
+	if(a%b==0)
+	return 1;
+	return 0;
+
+}
+
+int Max(int a, int b)
+{
+	if(a>b)
+	return a;
+	else return b;
+}
+
+int VMedio(int a, int b)
+{
+	return (a+b)/2;	
+}
---
+++ headers/func.h
@@ -0,0 +1,4 @@
+int EDivisivel(int a, int b);
+int Max(int a, int b);
+int VMedio(int a, int b);
+
